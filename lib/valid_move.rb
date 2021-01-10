aad1a80db74071c41493098c6b366b4181771d54
Sarah Wostenberg <swostenberg@gmail.com>
2021-01-10 23:50:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if index.between?(0,8) 
-    puts "it's between 0-8"
-  if board[index] == "X" || board[index] == "O"
-    puts "not taken"
+  index.between?(0,8)
+  board[index] == "X" || board[index] == "O"
 end 
 
 
