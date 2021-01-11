845e05bbd066d5b4036f144417d845a1e4fd29f8
Sarah Wostenberg <swostenberg@gmail.com>
2021-01-11 03:28:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,14 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  index.between?(0,8) 
+  index.between?(0,8) && position_taken()
 end 
 
 def position_taken?(board, index)
   board[index] != "X" || board[index] != "O"
 end 
+
+
 
 
 
